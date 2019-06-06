74869e5306cc7c737c144ff50998776608d157ce
Jesús Rodríguez Pérez <jesus.rodriguezperez@amaris.com>
2019-06-06 09:20:00 +0200

    improved logs

--- clog/colors.go
+++ clog/colors.go
@@ -2,31 +2,48 @@ package clog
 
 import (
 	"github.com/fatih/color"
+	log "github.com/sirupsen/logrus"
+	"strconv"
 )
-
-const sufix = "\n"
 
 var error = color.New(color.FgRed).PrintfFunc()
 var success = color.New(color.FgGreen).PrintfFunc()
 var info = color.New(color.FgCyan).PrintfFunc()
 var debug = color.New().PrintfFunc()
 
+var logger = initLogger()
+var errorCounter = 0
+
+func initLogger() *log.Logger {
+	l := log.New()
+	l.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: true,
+		ForceColors:      true,
+	})
+	return l
+}
+
 // Success print success msg
 func Success(format string, a ...interface{}) {
-	success(format+sufix, a...)
+	//logrus.Debug()
+	//success(format+"\n", a...)
+	f := color.New(color.FgGreen).SprintfFunc()
+	logger.Debug(f(format, a...))
 }
 
 // Error print error msg
-func Error(format string, a ...interface{}) {
-	error(format+sufix, a...)
+func Error(format string, args ...interface{}) {
+	errorCounter++
+	error(strconv.Itoa(errorCounter)+" "+format+"\n", args...)
+	//logger.Error(args...)
 }
 
 // Info print info msg
 func Info(format string, a ...interface{}) {
-	info(format+sufix, a...)
+	info(format, a...)
 }
 
 // Debug print debug msg
 func Debug(format string, a ...interface{}) {
-	debug(format+sufix, a...)
+	debug(format, a...)
 }
